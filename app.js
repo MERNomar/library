6ed3c4f64b36f7acad67dde4f4598fb217bca915
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-09-15 14:02:38 +0300

    adding cookie parser

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express')
 const app = express()
 const routes = require('./routes/routes')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
 
-
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static(`public`))
